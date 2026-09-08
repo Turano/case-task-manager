7a4fddc45c69ae16ce12226350257e82b3ca55c5
Alice Carvalho Turano <turano@usp.br>
2026-09-08 19:41:27 -0300

    fix: fix loading layout on mobile
    - Also removed "Editar tarefa" text since it's used on both create and edit task components

--- components/loading-task-form.tsx
+++ components/loading-task-form.tsx
@@ -1,12 +1,11 @@
 export default function LoadingTaskForm() {
   return (
-    <div className="my-auto gap-4">
-      <h1 className="text-2xl font-bold text-center">Editar tarefa</h1>
-      <div className="flex flex-col gap-4 max-w-xl mx-auto">
+    <div className="my-auto">
+      <div className="flex flex-col gap-4 sm:max-w-xl sm:mx-auto">
         <div className="h-4 w-48 bg-gray-200 rounded" />
-        <div className="mt-2 h-8 w-xl bg-gray-200 rounded" />
+        <div className="mt-2 h-8 sm:w-xl bg-gray-200 rounded" />
         <div className="h-4 w-48 bg-gray-200 rounded" />
-        <div className="mt-2 h-10 w-xl bg-gray-200 rounded" />
+        <div className="mt-2 h-10 sm:w-xl bg-gray-200 rounded" />
       </div>
     </div>
   );
